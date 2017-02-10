4419b45b8dee03c43b43513c0d8b67b28385a2bb
Marcin Ziąbek <marcin.ziabek@uj.edu.pl>
2017-02-10 02:24:33 +0100

    added menu

--- NugetWebsiteModern/Controllers/HomeController.cs
+++ NugetWebsiteModern/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace NugetWebsiteModern.Controllers
 {
-    public class HomeeController : Controller
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
--- NugetWebsiteModern/Startup.cs
+++ NugetWebsiteModern/Startup.cs
@@ -19,6 +19,7 @@ namespace NugetWebsiteModern
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
             Configuration = builder.Build();
         }
 
